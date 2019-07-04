b25ffdbb788b773319e5b2236f796b4c8b14791f
David Douard <david.douard@sdfa3.org>
2019-07-04 14:54:36 +0200

    tests: adapt to aiohttp 3
    - resource.url() has been deprecated since 1.1.0 and removed in 3.0.0,
    - Runner() needs a app_uri argument.
    
    tests: fix test_runner for aiohttp 3

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -7,7 +7,8 @@ class TestRunner:
 
     def test_bind(self):
         app = web.Application()
-        runner = Runner(app, host='1.2.3.4', port=5678)
+        runner = Runner(app, host='1.2.3.4', port=5678,
+                        app_uri='http://1.2.3.4:5678')
         assert runner.bind == '1.2.3.4:5678'
 
     def test_app_uri_is_required_for_reload(self):
